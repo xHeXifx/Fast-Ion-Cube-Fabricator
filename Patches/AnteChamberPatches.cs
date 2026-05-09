43653b77f3c19a3a3a3b6443ead5564292ceb4d0
HeXif <100049779+xHeXifx@users.noreply.github.com>
2026-05-09 23:33:46 +0100

    SafeScan (Divide by 0 fix)
    Took into account users are able to enter "0" in the config. This would crash as the in-game logic divides our value to determine how long to regen for. Our fix chooses the largest out of 0.1 and the value, therefore negative numbers and 0 shouldnt work.

--- Patches/AnteChamberPatches.cs
+++ Patches/AnteChamberPatches.cs
@@ -1,17 +1,20 @@
 using HarmonyLib;
 using System.Collections.Generic;
 using System.Reflection.Emit;
+using UnityEngine;
 
 [HarmonyPatch(typeof(AnteChamber), "UpdateScanAmountMaterials")]
 public class AnteChamber_ScanSpeedPatch
 {
     static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
     {
+        float safeScanTime = Mathf.Max(0.1f, IonCubeSpeedMod.ScanTime.Value);
+
         foreach (var code in instructions)
         {
             if (code.opcode == OpCodes.Ldc_R4 && code.operand is float f && f == 120f)
             {
-                code.operand = IonCubeSpeedMod.ScanTime.Value;
+                code.operand = safeScanTime;
             }
 
             yield return code;
